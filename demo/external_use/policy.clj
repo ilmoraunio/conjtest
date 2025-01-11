e4a87bfa18f9ade7022c404b1a33e01b4e9f636c
Ilmo Raunio <ilmo@raunio.email>
2025-01-12 01:21:59 +0200

    Add external use demo

---
+++ demo/external_use/policy.clj
@@ -0,0 +1,13 @@
+(ns policy)
+
+(defn deny-incorrect-log-level-development
+  [input]
+  (when (and (= :development (:env input))
+             (not= :debug (:log input)))
+    "Applications in the development environment should have debug logging"))
+
+(defn deny-incorrect-log-level-production
+  [input]
+  (when (and (= :production (:env input))
+             (not= :error (:log input)))
+    "Applications in the production environment should have error only logging"))
