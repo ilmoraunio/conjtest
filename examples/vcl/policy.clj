819165fc05f17433e8be3731bd1e6d3e9f3fb52a
Ilmo Raunio <ilmo@raunio.email>
2025-01-13 23:18:20 +0200

    fix vcl example

--- examples/vcl/policy.clj
+++ examples/vcl/policy.clj
@@ -3,12 +3,10 @@
 (defn deny-purge-list
   [input]
   (let [allowlist #{"127.0.0.1" "localhost"}
-        purges (get-in input ["acl" "purge"])
-        violations (for [purge purges
-                         :when (not (allowlist purge))]
-                     purge)]
-    (when (not-empty violations)
-      (format "acl purge should be one of %s, was %s instead" (into [] allowlist) purges))))
+        purges (get-in input ["acl" "purge"])]
+    (for [purge purges
+          :when (not (allowlist purge))]
+      (format "acl purge should be one of %s, was %s instead" (into [] allowlist) purge))))
 
 (defn deny-incorrect-port
   [input]
