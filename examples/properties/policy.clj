d52393f7bf4d3d61227a5a31187f661db269974a
Ilmo Raunio <ilmo@raunio.email>
2025-01-13 23:12:49 +0200

    fix properties examples

--- examples/properties/policy.clj
+++ examples/properties/policy.clj
@@ -2,16 +2,14 @@
 
 (defn deny-invalid-uri
   [input]
-  (when-let [matches (seq (for [[key value] input
-                                :when (and (clojure.string/includes? (clojure.string/lower-case key) "url")
-                                           (not (clojure.string/includes? (clojure.string/lower-case value) "http")))]
-                            (format "Must have a valid uri defined '%s'" value)))]
-    (clojure.string/join ", " matches)))
+  (for [[key value] input
+        :when (and (clojure.string/includes? (clojure.string/lower-case key) "url")
+                   (not (clojure.string/includes? (clojure.string/lower-case value) "http")))]
+    (format "Must have a valid uri defined '%s'" value)))
 
 (defn deny-secrets
   [input]
-  (when-let [matches (seq (for [[key _value] input
-                                :when (and (not= "secret.value.exception" key)
-                                           (clojure.string/includes? (clojure.string/lower-case key) "secret"))]
-                            (format "'%s' may contain a secret value" key)))]
-    (clojure.string/join ", " matches)))
+  (for [[key _value] input
+        :when (and (not= "secret.value.exception" key)
+                   (clojure.string/includes? (clojure.string/lower-case key) "secret"))]
+    (format "'%s' may contain a secret value" key)))
