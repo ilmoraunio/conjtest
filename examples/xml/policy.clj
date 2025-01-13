7c797e7fa4db4635c6d4270cebabbe41791b0d37
Ilmo Raunio <ilmo@raunio.email>
2025-01-13 23:20:38 +0200

    fix XML examples

--- examples/xml/policy.clj
+++ examples/xml/policy.clj
@@ -3,25 +3,22 @@
 (defn deny-incorrect-compiler-plugin-version
   [input]
   (let [expected-version "3.6.1"]
-    (when-let [matches (seq (for [plugin (get-in input ["project" "build" "plugins" "plugin"])
-                                  :when (and (= (get plugin "artifactId") "maven-compiler-plugin")
-                                             (not= (get plugin "version") expected-version))]
-                              (format "maven-compiler-plugin must have the following version: %s" expected-version)))]
-      (clojure.string/join "\n" matches))))
+    (for [plugin (get-in input ["project" "build" "plugins" "plugin"])
+          :when (and (= (get plugin "artifactId") "maven-compiler-plugin")
+                     (not= (get plugin "version") expected-version))]
+      (format "maven-compiler-plugin must have the following version: %s" expected-version))))
 
 (defn deny-missing-instrument-goal
   [input]
-  (when-let [matches (seq (for [plugin (get-in input ["project" "build" "plugins" "plugin"])
-                            :when (and (= (get plugin "artifactId") "activejdbc-instrumentation")
-                                       (not= "instrument" (get-in plugin ["executions" "execution" "goals" "goal"])))]
-                        (format "activejdbc-instrumentation plugin must have 'instrument' goal")))]
-    (clojure.string/join "\n" matches)))
+  (for [plugin (get-in input ["project" "build" "plugins" "plugin"])
+        :when (and (= (get plugin "artifactId") "activejdbc-instrumentation")
+                   (not= "instrument" (get-in plugin ["executions" "execution" "goals" "goal"])))]
+    (format "activejdbc-instrumentation plugin must have 'instrument' goal")))
 
 (defn deny-incorrect-surefire-plugin-version
   [input]
   (let [expected-version "2.18.1"]
-    (when-let [matches (seq (for [plugin (get-in input ["project" "build" "plugins" "plugin"])
-                                  :when (and (= (get plugin "artifactId") "maven-surefire-plugin")
-                                             (not= (get plugin "version") expected-version))]
-                              (format "maven-surefire-plugin must have the following version: %s" expected-version)))]
-      matches)))
+    (for [plugin (get-in input ["project" "build" "plugins" "plugin"])
+          :when (and (= (get plugin "artifactId") "maven-surefire-plugin")
+                     (not= (get plugin "version") expected-version))]
+      (format "maven-surefire-plugin must have the following version: %s" expected-version))))
