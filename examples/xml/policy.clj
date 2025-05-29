9edd26aec0f386e516ca54daca0e93c410202a60
Ilmo Raunio <ilmo@raunio.email>
2025-05-30 00:04:01 +0300

    fix: xml tests

--- examples/xml/policy.clj
+++ examples/xml/policy.clj
@@ -3,22 +3,22 @@
 (defn deny-incorrect-compiler-plugin-version
   [input]
   (let [expected-version "3.6.1"]
-    (for [plugin (get-in input ["project" "build" "plugins" "plugin"])
-          :when (and (= (get plugin "artifactId") "maven-compiler-plugin")
-                     (not= (get plugin "version") expected-version))]
+    (for [plugin (get-in input [:project :build :plugins :plugin])
+          :when (and (= (:artifactId plugin) "maven-compiler-plugin")
+                     (not= (:version plugin) expected-version))]
       (format "maven-compiler-plugin must have the following version: %s" expected-version))))
 
 (defn deny-missing-instrument-goal
   [input]
-  (for [plugin (get-in input ["project" "build" "plugins" "plugin"])
-        :when (and (= (get plugin "artifactId") "activejdbc-instrumentation")
-                   (not= "instrument" (get-in plugin ["executions" "execution" "goals" "goal"])))]
+  (for [plugin (get-in input [:project :build :plugins :plugin])
+        :when (and (= (:artifactId plugin) "activejdbc-instrumentation")
+                   (not= "instrument" (get-in plugin [:executions :execution :goals :goal])))]
     (format "activejdbc-instrumentation plugin must have 'instrument' goal")))
 
 (defn deny-incorrect-surefire-plugin-version
   [input]
   (let [expected-version "2.18.1"]
-    (for [plugin (get-in input ["project" "build" "plugins" "plugin"])
-          :when (and (= (get plugin "artifactId") "maven-surefire-plugin")
-                     (not= (get plugin "version") expected-version))]
+    (for [plugin (get-in input [:project :build :plugins :plugin])
+          :when (and (= (:artifactId plugin) "maven-surefire-plugin")
+                     (not= (:version plugin) expected-version))]
       (format "maven-surefire-plugin must have the following version: %s" expected-version))))
