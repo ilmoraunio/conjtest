1d7e442c7ba504734f9c80670f854e33234b5847
Ilmo Raunio <ilmo@raunio.email>
2025-05-29 23:56:37 +0300

    fix: properties tests

--- examples/properties/policy.clj
+++ examples/properties/policy.clj
@@ -3,13 +3,13 @@
 (defn deny-invalid-uri
   [input]
   (for [[key value] input
-        :when (and (clojure.string/includes? (clojure.string/lower-case key) "url")
+        :when (and (clojure.string/includes? (clojure.string/lower-case (name key)) "url")
                    (not (clojure.string/includes? (clojure.string/lower-case value) "http")))]
     (format "Must have a valid uri defined '%s'" value)))
 
 (defn deny-secrets
   [input]
   (for [[key _value] input
-        :when (and (not= "secret.value.exception" key)
-                   (clojure.string/includes? (clojure.string/lower-case key) "secret"))]
+        :when (and (not= "secret.value.exception" (name key))
+                   (clojure.string/includes? (clojure.string/lower-case (name key)) "secret"))]
     (format "'%s' may contain a secret value" key)))
