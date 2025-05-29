a4e11ecabee02b6f87b0d594affd3889ce5f1975
Ilmo Raunio <ilmo@raunio.email>
2025-05-30 00:02:14 +0300

    fix: vcl tests

--- examples/vcl/policy.clj
+++ examples/vcl/policy.clj
@@ -3,12 +3,12 @@
 (defn deny-purge-list
   [input]
   (let [allowlist #{"127.0.0.1" "localhost"}
-        purges (get-in input ["acl" "purge"])]
+        purges (get-in input [:acl :purge])]
     (for [purge purges
           :when (not (allowlist purge))]
       (format "acl purge should be one of %s, was %s instead" (into [] allowlist) purge))))
 
 (defn deny-incorrect-port
   [input]
-  (when (not= "8080" (get-in input ["backend" "app" "port"]))
+  (when (not= "8080" (get-in input [:backend :app :port]))
     "default backend port should be 8080"))
