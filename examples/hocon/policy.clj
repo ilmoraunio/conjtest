df544f826c64b17f78ac4c7e18689136b42d28ee
Ilmo Raunio <ilmo@raunio.email>
2025-05-29 23:49:21 +0300

    fix: hocon tests

--- examples/hocon/policy.clj
+++ examples/hocon/policy.clj
@@ -2,10 +2,10 @@
 
 (defn deny-wrong-port
   [input]
-  (when (not= 9000.0 (get-in input ["play" "server" "http" "port"]))
+  (when (not= 9000.0 (get-in input [:play :server :http :port]))
     "Play http server port should be 9000"))
 
 (defn deny-http-bind-address
   [input]
-  (when (not= "0.0.0.0" (get-in input ["play" "server" "http" "address"]))
+  (when (not= "0.0.0.0" (get-in input [:play :server :http :address]))
     "Play http server bind address should be 0.0.0.0"))
