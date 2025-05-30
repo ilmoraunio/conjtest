e390baf19548a4ec4e8ca52fd5a61eb3362ac748
Ilmo Raunio <ilmo@raunio.email>
2025-05-30 22:29:13 +0300

    fix: yaml kubernetes tests

--- examples/yaml/kubernetes/policy_go.clj
+++ examples/yaml/kubernetes/policy_go.clj
@@ -2,28 +2,28 @@
 
 (defn- deployment?
   [input]
-  (= "Deployment" (get-in input ["kind"])))
+  (= "Deployment" (:kind input)))
 
 (defn deny-should-not-run-as-root
   [input]
-  (let [name (get-in input ["metadata" "name"])]
+  (let [name (get-in input [:metadata :name])]
     (when (and (deployment? input)
-               (not (true? (get-in input ["spec"
-                                          "template"
-                                          "spec"
-                                          "securityContext"
-                                          "runAsNonRoot"]))))
+               (not (true? (get-in input [:spec
+                                          :template
+                                          :spec
+                                          :securityContext
+                                          :runAsNonRoot]))))
       (format "Containers must not run as root in Deployment \"%s\"" name))))
 
 (defn- contains-required-deployment-selectors?
   [input]
-  (let [match-labels (get-in input ["spec" "selector" "matchLabels"])]
-    (and (get match-labels "app")
-         (get match-labels "release"))))
+  (let [match-labels (get-in input [:spec :selector :matchLabels])]
+    (and (:app match-labels)
+         (:release match-labels))))
 
 (defn deny-missing-required-deployment-selectors
   [input]
-  (let [name (get-in input ["metadata" "name"])]
+  (let [name (get-in input [:metadata :name])]
     (when (and (deployment? input)
                (not (contains-required-deployment-selectors? input)))
       (format "Deployment \"%s\" must provide app/release labels for pod selectors" name))))
