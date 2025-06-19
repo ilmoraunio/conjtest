e04e6919a85bf6cce3ff95cab1d60865eac4dbe7
Ilmo Raunio <ilmo@raunio.email>
2025-06-19 23:52:59 +0300

    add json example

--- examples/json/policy.clj
+++ examples/json/policy.clj
@@ -5,3 +5,10 @@
   (for [[_lib-name lib-version :as lib] (:dependencies input)
         :when (clojure.string/starts-with? lib-version "^")]
     (format "caret ranges not allowed, offending library: %s" lib)))
+
+(def allow-declarative-example
+  [:map
+   [:dependencies [:map-of :keyword [:re
+                                     {:error/fn (fn [{:keys [value]} _]
+                                                  (format "caret ranges not allowed, offending library: %s" value))}
+                                     "^[0-9~(>=)(latest)(beta)]"]]]])
